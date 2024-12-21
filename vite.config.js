43ebd6e2b5475f344927899256dbb1bb71e055a8
Murugan-S1234 <murugansiva2193@gmail.com>
2024-12-22 00:12:42 +0530

    Fixed local build issues

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure this is set to 'dist'
+    outDir: 'dist', // Output directory for production build
   },
-
+  server: {
+    port: 3000, // Development server port
+  },
 });
-s
